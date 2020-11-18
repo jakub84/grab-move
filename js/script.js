01fb1e1ab589f47746caf9ef215b3b5e046bf088
Jakub <jakub.d84@gmail.com>
2020-11-18 22:47:12 +0100

    add basics

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('test');
+const container = document.querySelector('.box-container');
+let mouseDown = false;
+let startX;
+let scrollLeft;
+
+container.addEventListener("mousedown", () => {
+    console.log("down")
+})
+
+container.addEventListener("mouseleave", () => {
+    console.log("leave")
+})
+
+container.addEventListener("mouseup", () => {
+    console.log("up")
+})
+
+container.addEventListener("mousemove", () => {
+    console.log("move")
+})
