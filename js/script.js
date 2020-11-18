a573f4da692f268403cc2bc6c101d6b1d109cee0
Jakub <jakub.d84@gmail.com>
2020-11-19 00:12:56 +0100

    add basic js

--- js/script.js
+++ js/script.js
@@ -3,18 +3,32 @@ let mouseDown = false;
 let startX;
 let scrollLeft;
 
-container.addEventListener("mousedown", () => {
-    console.log("down")
+container.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    startX = e.pageX - container.offsetLeft;
+    console.log(startX);
+    scrollLeft = container.scrollLeft;
+    mouseDown = true;
+    container.classList.add('active');
 })
 
 container.addEventListener("mouseleave", () => {
-    console.log("leave")
+    console.log("leave");
+    mouseDown = false;
+    container.classList.remove('active');
 })
 
 container.addEventListener("mouseup", () => {
-    console.log("up")
+    console.log("up");
+    mouseDown = false;
+    container.classList.remove('active');
 })
 
-container.addEventListener("mousemove", () => {
-    console.log("move")
+container.addEventListener("mousemove", (e) => {
+    if(!mouseDown) return;
+    
+    console.log("moving");
+    x = e.pageX - container.offsetLeft;
+    const walk = (x - startX) * 2;
+    container.scrollLeft = scrollLeft - walk;
 })
